575d47c8f0a7de58de6ac083476cbf5f1c524391
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-02-19 11:06:15 +0530

    Varibles
    Declare Variables in php

---
+++ new 1.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+echo "Hello World!";//single line comment
+/*this is multiline
+comments*/
+echo"<br>";
+$color="red";
+echo $color;
+echo"<br>";
+echo "My Car is ".$color;
+echo"<br>";
+
+$txt="Hello";
+echo $txt;
+echo"<br>";
+$x=10;
+$y=15;
+echo $x+$y;
+echo"<br>";
+$a="Gayathri";
+echo "My name is $a";
+echo"<br>";
+echo"My name is ".$a;
+echo"<br>";
+echo"Gayathri "."Buddhika";
+?>
+</body>
+</html>
---
+++ new.php
@@ -0,0 +1,3 @@
+</<?php 
+echo "Gayathri "."Buddhika";
+ ?>
